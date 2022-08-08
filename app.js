2a2e3884e1ae62c57e2014ed7c7a27119e29074f
oliver <oltepper@gmail.com>
2022-08-08 10:39:31 +0200

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDate();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.send("Yay it's the weekend!");
+  } else {
+    res.send('Boo! I have to work!');
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000.');
+});
