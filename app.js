e31d34097da5510fe8c7eaf9a154abb08120a958
Oliver <oltepper@gmail.com>
2022-08-09 12:47:31 +0200

    update

--- app.js
+++ app.js
@@ -2,47 +2,37 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const ejs = require('ejs');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+var items = [];
 
-app.get('/', function (req, res) {
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Response current Date to the Website
+app.get('/', (req, res) => {
   
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = '';
-
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday ';
-      break;
-    case 1:
-      day = 'Monday ';
-      break;
-    case 2:
-      day = 'Tuesday ';
-      break;
-    case 3:
-      day = 'Wednesday ';
-      break;
-    case 4:
-      day = 'Thursday ';
-      break;
-    case 5:
-      day = 'Friday ';
-      break;
-    case 6:
-      day = 'Saturday ';
-      break;
-    default:
-      console.log('Error');
-  }
   
-  res.render('list', { kindOfDay: day });
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  var day = today.toLocaleDateString('de-DE', options);
+  res.render('list', { kindOfDay: day, newListItems: items });
 });
 
-app.listen(3000, function () {
+// Get user input from the form
+app.post('/', (req, res) => {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
   console.log('Server started on port 3000.');
 });
